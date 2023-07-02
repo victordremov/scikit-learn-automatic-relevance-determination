e92497c6288a95b9aad059b26499e7fd9633280d
Viktor Dremov <victor.dremov@gmail.com>
2023-07-02 19:26:21 +0200

    new model parameter: alpha_max
    when alpha[i] > alpha_max, assume that feature irrelevant, freeze w[i] = 0 and stop to update alpha[i]

--- ARD/ARDRegressor.py
+++ ARD/ARDRegressor.py
@@ -9,12 +9,16 @@ from sklearn.metrics import euclidean_distances
 
 
 class ARDRegressor(BaseEstimator):
-    """ Automatic relevance determination
+    """Automatic relevance determination
 
     Parameters
     ----------
-    n_iterations : int, default = 10
+    n_iterations : int, default = 100
         Number of iterations when converging
+    max_alpha: float, default = 100_000
+        Maximum value of individual feature regularization parameter.
+        When alpha[i] is greater than max_alpha, feature[i] is treated as unrelevant.
+
 
     Examples
     --------
@@ -26,15 +30,17 @@ class ARDRegressor(BaseEstimator):
     >>> estimator.fit(X, y)
     ARDRegressor()
     """
-    def __init__(self, n_iterations: int = 100):
-        self.n_iterations = n_iterations
 
-    def fit(self, X, y):
+    def __init__(self, n_iterations: int = 100, max_alpha: float = 100_000):
+        self.n_iterations = n_iterations
+        self.max_alpha = max_alpha
+
+    def fit(self, X_relevant, y):
         """A reference implementation of a fitting function.
 
         Parameters
         ----------
-        X : array-like, shape (n_samples, n_features)
+        X_relevant : array-like, shape (n_samples, n_features)
             The training input samples.
         y : array-like, shape (n_samples,)
             The target values (class labels in classification, real numbers in
@@ -46,46 +52,57 @@ class ARDRegressor(BaseEstimator):
             Returns self.
         """
         assert self.n_iterations > 0
-        X, y = check_X_y(X, y, accept_sparse=False, dtype=np.float64)
+        X_relevant, y = check_X_y(X_relevant, y, accept_sparse=False, dtype=np.float64)
+        X_original = np.asarray(X_relevant)
+        n, d = X_original.shape
+
         y = y.astype(np.float64)
-        n, d = X.shape
         t = y.reshape(n, 1)
 
         alphas_old = np.ones((d, 1), dtype=np.float64)
-        beta_old = 1.
-
-        XT_dot_t = X.T @ t
-        XT_dot_X = X.T @ X
-
+        beta_old = 1.0
         for iteration in range(self.n_iterations):
+            relevant_features = np.arange(d)[alphas_old.ravel() <= self.max_alpha]
+            X_relevant = X_original[:, relevant_features]
+            XT_dot_t = X_relevant.T @ t
+            XT_dot_X = X_relevant.T @ X_relevant
             # update sigma, w_mp when A, b is fixed
             # as parameters of the posterior distribution
             # p(w | X, T, A, beta) ~ N(w | w_mp, sigma^(-1))
-            sigma = np.linalg.inv(beta_old * XT_dot_X + np.diag(alphas_old.reshape(-1)))
-            w_mp = beta_old * sigma @ XT_dot_t
+            sigma_relevant = np.linalg.inv(
+                beta_old * XT_dot_X + np.diag(alphas_old[relevant_features].reshape(-1))
+            )
+            w_mp = np.zeros((d, 1), dtype=np.float64)
+            w_mp[relevant_features] = beta_old * sigma_relevant @ XT_dot_t
 
             # update A and beta by maximizing variadic lower estimate for fixed sigma, w_mp
-            sigma_diag = np.diag(sigma).reshape(d, 1)
-            beta_new = (n - (1.0 - alphas_old.T) @ sigma_diag).item() / np.linalg.norm(t - X @ w_mp) ** 2
-            alphas_new = (1.0 + alphas_old * sigma_diag) / w_mp ** 2
-            if not np.isfinite(beta_new) or not np.isfinite(alphas_new).all():
+            sigma_relevant_diag = np.diag(sigma_relevant).reshape(-1, 1)
+            beta_new = (
+                n - (1.0 - alphas_old[relevant_features].T) @ sigma_relevant_diag
+            ).item() / np.linalg.norm(t - X_relevant @ w_mp[relevant_features]) ** 2
+            alphas_new = alphas_old.copy()
+            alphas_new[relevant_features] = (
+                1.0 + alphas_old[relevant_features] * sigma_relevant_diag
+            ) / w_mp[relevant_features] ** 2
+            if not np.isfinite(beta_new):
+                # failed to converge
                 break
-
             alphas_old = alphas_new
             beta_old = beta_new
 
-        self.w_ = w_mp
-        self.sigma_ = sigma
-        self.alphas_ = alphas_old
-        self.beta_ = beta_old
+        self.w_ = np.zeros((d, 1), dtype=np.float64)
+        self.w_[relevant_features] = w_mp[relevant_features]
+
+        self.sigma_relevant_ = sigma_relevant
+        self.alphas_ = alphas_new
+        self.beta_ = beta_new
         self.n_features_in_ = d
 
         self.is_fitted_ = True
-        # `fit` should always return `self`
         return self
 
     def predict(self, X):
-        """ A reference implementation of a predicting function.
+        """A reference implementation of a predicting function.
 
         Parameters
         ----------
@@ -98,12 +115,12 @@ class ARDRegressor(BaseEstimator):
             Returns an array of ones.
         """
         X = check_array(X, accept_sparse=False, dtype=np.float64)
-        check_is_fitted(self, 'is_fitted_')
+        check_is_fitted(self, "is_fitted_")
         return X @ self.w_
 
 
 class TemplateClassifier(ClassifierMixin, BaseEstimator):
-    """ An example classifier which implements a 1-NN algorithm.
+    """An example classifier which implements a 1-NN algorithm.
 
     For more information regarding how to build your own classifier, read more
     in the :ref:`User Guide <user_guide>`.
@@ -122,7 +139,8 @@ class TemplateClassifier(ClassifierMixin, BaseEstimator):
     classes_ : ndarray, shape (n_classes,)
         The classes seen at :meth:`fit`.
     """
-    def __init__(self, demo_param='demo'):
+
+    def __init__(self, demo_param="demo"):
         self.demo_param = demo_param
 
     def fit(self, X, y):
@@ -151,7 +169,7 @@ class TemplateClassifier(ClassifierMixin, BaseEstimator):
         return self
 
     def predict(self, X):
-        """ A reference implementation of a prediction for a classifier.
+        """A reference implementation of a prediction for a classifier.
 
         Parameters
         ----------
@@ -165,7 +183,7 @@ class TemplateClassifier(ClassifierMixin, BaseEstimator):
             seen during fit.
         """
         # Check is fit had been called
-        check_is_fitted(self, ['X_', 'y_'])
+        check_is_fitted(self, ["X_", "y_"])
 
         # Input validation
         X = check_array(X)
@@ -175,7 +193,7 @@ class TemplateClassifier(ClassifierMixin, BaseEstimator):
 
 
 class TemplateTransformer(TransformerMixin, BaseEstimator):
-    """ An example transformer that returns the element-wise square root.
+    """An example transformer that returns the element-wise square root.
 
     For more information regarding how to build your own transformer, read more
     in the :ref:`User Guide <user_guide>`.
@@ -190,7 +208,8 @@ class TemplateTransformer(TransformerMixin, BaseEstimator):
     n_features_ : int
         The number of features of the data passed to :meth:`fit`.
     """
-    def __init__(self, demo_param='demo'):
+
+    def __init__(self, demo_param="demo"):
         self.demo_param = demo_param
 
     def fit(self, X, y=None):
@@ -217,7 +236,7 @@ class TemplateTransformer(TransformerMixin, BaseEstimator):
         return self
 
     def transform(self, X):
-        """ A reference implementation of a transform function.
+        """A reference implementation of a transform function.
 
         Parameters
         ----------
@@ -231,7 +250,7 @@ class TemplateTransformer(TransformerMixin, BaseEstimator):
             in ``X``.
         """
         # Check is fit had been called
-        check_is_fitted(self, 'n_features_')
+        check_is_fitted(self, "n_features_")
 
         # Input validation
         X = check_array(X, accept_sparse=True)
@@ -239,6 +258,7 @@ class TemplateTransformer(TransformerMixin, BaseEstimator):
         # Check that the input is of the same shape as the one passed
         # during fit.
         if X.shape[1] != self.n_features_:
-            raise ValueError('Shape of input is different from what was seen'
-                             'in `fit`')
+            raise ValueError(
+                "Shape of input is different from what was seen" "in `fit`"
+            )
         return np.sqrt(X)
